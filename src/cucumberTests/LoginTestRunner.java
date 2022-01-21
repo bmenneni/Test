b4f56a87dff2cd6d5abb2ba26dcdaab2294e16c5
Ravi Menneni <rmohanmenneni@gmail.com>
2022-01-20 20:56:54 -0600

    commiting crap

--- src/cucumberTests/LoginTestRunner.java
+++ src/cucumberTests/LoginTestRunner.java
@@ -1,11 +1,10 @@
 package cucumberTests;
 
 import org.junit.runner.RunWith;
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.Cucumber;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features="features",glue="stepImplementations")
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(features="features",glue="stepImplementations")
 public class LoginTestRunner {
 
 }
